9c527be24a239c07d69948f8d9fec392ca7af12c
ceesharptech <eniolaamusu6@gmail.com>
2026-09-25 10:43:56 +0100

    adding tally link CTA

--- lib/constants.ts
+++ lib/constants.ts
@@ -5,5 +5,4 @@
  * link to this destination. Replace with your real email or scheduling URL
  * when ready.
  */
-export const DEMO_MAILTO_URL =
-  "mailto:hello@onboard360.app?subject=Demo%20Request";
+export const DEMO_MAILTO_URL = "https://tally.so/r/D4QZgR";
